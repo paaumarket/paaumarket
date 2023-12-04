0b77bec53472798ed0741af2b6b02b893f9c7557
Abel Emmanuel <mannydev02@gmail.com>
2023-12-04 15:12:32 +0100

    Migrate to vite framework

--- services/useFetch.js
+++ services/useFetch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function useFetch(method = "get", location = "/", data = {}) {
   try {
-    const url = `https://puce-tiny-clownfish.cyclic.app/api${location}`;
+    const url = `https://api-paaumarket.onrender.com/api${location}`;
 
     const result = await axios({ method, url, data });
 
